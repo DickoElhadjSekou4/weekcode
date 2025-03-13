98f053dd2e89255680ba3c46c51b670523a739ab
TiteDon <dontresor3005@gmail.com>
2025-03-13 12:38:06 +0000

    Update dataloader.py

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,5 +1,11 @@
 import pandas as pd 
+from ucimlrepo import fetch_ucirepo 
 
 def get_downloaded_dataset() -> pd.DataFrame:
     df = pd.read_csv(r"data\risk_factors_cervical_cancer.csv")
     return df 
+def load_dataset_from_ucirepo():
+    cervical_cancer_risk_factors = fetch_ucirepo(id=383) 
+    X = cervical_cancer_risk_factors.data.features 
+    y = cervical_cancer_risk_factors.data.targets 
+    return X, y
