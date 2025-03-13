f70d75f61173d6be2330a3bc602ae94be5a8040e
DickoElhadjSekou4 <elhadjsekoudicko3@gmail.com>
2025-03-13 13:22:05 +0000

    test both dataloaders

--- src/dataloader.py
+++ src/dataloader.py
@@ -4,8 +4,14 @@ from ucimlrepo import fetch_ucirepo
 def get_downloaded_dataset() -> pd.DataFrame:
     df = pd.read_csv(r"data\risk_factors_cervical_cancer.csv")
     return df 
+
 def load_dataset_from_ucirepo():
     cervical_cancer_risk_factors = fetch_ucirepo(id=383) 
     X = cervical_cancer_risk_factors.data.features 
-    y = cervical_cancer_risk_factors.data.targets 
-    return X, y
+    # y = cervical_cancer_risk_factors.data.targets 
+    df = pd.DataFrame(X)
+    return df
+
+if __name__ == "__main__":
+    df1 = get_downloaded_dataset()
+    df2 = load_dataset_from_ucirepo()
