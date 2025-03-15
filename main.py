b985e0b3c241fffcb5560907b44baef6bca88d95
SaydAhmed222 <meitesayd2222@gmail.com>
2025-03-15 14:47:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from utils.data_preprocessing import load_and_clean_data, preprocess_data
+from utils.data_preprocessing import load_and_clean_data
 from models.random_forest import train_model
 from evaluation import evaluate_model
 from shap_analysis import analyze_shap
@@ -11,7 +11,7 @@ df = load_and_clean_data()
 X_train, X_test, y_train, y_test = preprocess_data(df)
 
 # Entraîner le modèle
-svm_model = train_svm(X_train, y_train)
+train_random_forest = train_model(X_train, y_train)
 
 # Évaluer le modèle
 evaluate_model(svm_model, X_test, y_test)
