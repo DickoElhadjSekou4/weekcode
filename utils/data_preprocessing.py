b446aa755fefa3bc74180cf6d77b6a2af7d95871
SaydAhmed222 <meitesayd2222@gmail.com>
2025-03-15 14:25:54 +0000

    Update data_preprocessing.py

--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
+from ucimlrepo import fetch_ucirepo
 
 def get_data():
     from sklearn.impute import SimpleImputer
